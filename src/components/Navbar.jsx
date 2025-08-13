cb923cefbbed7ab12c88411e783033ebaf11d463
Ayusherhjyuk <ayushjadhao8804@gmail.com>
2025-08-13 23:58:48 +0530

    ui updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   const links = [
     { name: "Dashboard", path: "/" },
     { name: "Portfolio", path: "/portfolio" },
-    { name: "Markets", path: "/markets" },
+    
   ];
 
   useEffect(() => {
@@ -26,16 +26,17 @@ export default function Navbar() {
       className={`sticky top-0 z-50 px-6 py-5 flex justify-between items-center transition-all duration-300
         ${isScrolled
           ? "bg-white/20 dark:bg-gray-800/30 backdrop-blur-lg border-b border-white/10 shadow-lg"
-          : "bg-white dark:bg-gray-800 shadow-md"
+          : " bg-white dark:bg-gray-800 shadow-md"
         }`}
     >
       {/* Left - Logo */}
-      <div className="flex items-center gap-2">
-        <FaBitcoin className="text-yellow-500 text-2xl" />
-        <span className="font-bold text-xl text-gray-900 dark:text-white tracking-tight">
-          CryptoDash
-        </span>
-      </div>
+<Link to="/" className="flex items-center gap-2">
+  <FaBitcoin className="text-yellow-500 text-2xl" />
+  <span className="font-bold text-xl text-gray-900 dark:text-white tracking-tight">
+    CryptoDash
+  </span>
+</Link>
+
 
       {/* Right - Links */}
       <div className="flex space-x-6">
@@ -43,10 +44,10 @@ export default function Navbar() {
           <Link
             key={link.path}
             to={link.path}
-            className={`relative font-medium transition-colors duration-200 ${
+            className={`relative font-medium  right-5 transition-colors duration-200 ${
               location.pathname === link.path
                 ? "text-blue-500"
-                : "text-gray-600 dark:text-gray-200 hover:text-black"
+                : "text-gray-950 dark:text-gray-200 hover:text-black"
             }`}
           >
             {link.name}
